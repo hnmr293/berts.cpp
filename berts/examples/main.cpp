4d2126adbec246025ae8a442f98ff5a6f996bf11
hnmr <pute.tk@gmail.com>
2023-12-24 03:38:18 +0900

    quantization test

--- berts/examples/main.cpp
+++ berts/examples/main.cpp
@@ -10,14 +10,22 @@ int main() {
 
     auto ctx = berts_load_from_file(model_path);
 
-    if (ctx) {
-        std::cout << "success" << std::endl;
-    } else {
-        std::cout << "fail" << std::endl;
+    if (!ctx) {
+        std::cout << "fail to open base model" << std::endl;
+        return 0;
     }
     
     berts_free(ctx);
 
+    const char *q8_path = ".gguf/bert-base-cased_q8.gguf";
+
+    bool ok = berts_model_quantize(model_path, q8_path, ggml_type::GGML_TYPE_Q8_0);
+
+    if (!ok) {
+        std::cout << "fail to quantize model" << std::endl;
+        return 0;
+    }
+    
     std::cout << "done" << std::endl;
     
     return 0;
