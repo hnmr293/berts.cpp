520718b563e036d378898e765cabae82cc7dea36
hnmr <pute.tk@gmail.com>
2023-12-30 18:38:26 +0900

    update test_tokenizer.cpp

--- berts/tests/test_tokenizer.cpp
+++ berts/tests/test_tokenizer.cpp
@@ -13,7 +13,7 @@ int main() {
     assert(ctx);
 
     berts_set_log_level(BERTS_LOG_ALL);
-    
+
     //
     // tokens
     //
@@ -181,11 +181,45 @@ int main() {
     // tokenize
     //
 
-    const std::string text1 = berts::fmt::fmt("{}Hi, I am {} man.", cls, mask);
-    size_t size = text1.size();
-    std::unique_ptr<bert_token_t[]> tokens{new bert_token_t[size]};
-    bool ok = berts_tokenize(ctx, text1.c_str(), tokens.get(), &size);
-    assert(ok);
+    {
+        const std::string text1 = berts::fmt::fmt("Hi, I am {} man.", mask);
+        size_t size = text1.size();
+        std::unique_ptr<bert_token_t[]> tokens{new bert_token_t[size]};
+        bool ok = berts_tokenize(ctx, text1.c_str(), tokens.get(), &size);
+        assert(ok);
+        assert(size == 9);
+        assert(tokens[0] == 101);  // [CLS]
+        assert(tokens[1] == 8790); // Hi
+        assert(tokens[2] == 117);  // ,
+        assert(tokens[3] == 146);  // I
+        assert(tokens[4] == 1821); // am
+        assert(tokens[5] == 103);  // [MASK]
+        assert(tokens[6] == 1299); // man
+        assert(tokens[7] == 119);  // .
+        assert(tokens[8] == 102);  // [SEP]
+    }
+
+    {
+        const std::string text1 = berts::fmt::fmt("Hi, I am {} man. How are you?", mask);
+        size_t size = text1.size();
+        std::unique_ptr<bert_token_t[]> tokens{new bert_token_t[size]};
+        bool ok = berts_tokenize(ctx, text1.c_str(), tokens.get(), &size);
+        assert(ok);
+        assert(size == 13);
+        assert(tokens[0] == 101);   // [CLS]
+        assert(tokens[1] == 8790);  // Hi
+        assert(tokens[2] == 117);   // ,
+        assert(tokens[3] == 146);   // I
+        assert(tokens[4] == 1821);  // am
+        assert(tokens[5] == 103);   // [MASK]
+        assert(tokens[6] == 1299);  // man
+        assert(tokens[7] == 119);   // .
+        assert(tokens[8] == 1731);  // How
+        assert(tokens[9] == 1132);  // are
+        assert(tokens[10] == 1128); // you
+        assert(tokens[11] == 136);  // ?
+        assert(tokens[12] == 102);  // [SEP]
+    }
 
     return 0;
 }
