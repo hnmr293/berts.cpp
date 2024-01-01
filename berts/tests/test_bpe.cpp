5055273ff734e339c80dff4e5c3a8fecb9727916
hnmr <pute.tk@gmail.com>
2024-01-02 01:42:26 +0900

    add test for bpe

---
+++ berts/tests/test_bpe.cpp
@@ -0,0 +1,163 @@
+#include <cassert>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include "berts/models/bpe.hpp"
+#include "berts/models/unicode.hpp"
+
+using ustr = berts::unicode::ustr;
+
+static inline std::vector<ustr> tokenize(const berts::bpe &bpe, const ustr &token) {
+    std::vector<ustr> result{};
+    bool ok = bpe.tokenize(token, result);
+    assert(ok);
+    return result;
+}
+
+int main() {
+    // unfused <unk>
+    {
+        berts::bpe bpe{"<unk>", 0.0, false};
+        std::unordered_map<ustr, size_t> vocab{{
+            {ustr{"a"}, 0},
+            {ustr{"b"}, 1},
+        }};
+        std::vector<std::pair<ustr, ustr>> merge{};
+        bool ok = bpe.load_vocab(vocab, merge);
+        assert(ok);
+
+        {
+            auto r = tokenize(bpe, "c");
+            assert(r.size() == 1);
+            assert(r[0] == ustr{"<unk>"});
+        }
+
+        {
+            auto r = tokenize(bpe, "cc");
+            assert(r.size() == 2);
+            assert(r[0] == ustr{"<unk>"});
+            assert(r[1] == ustr{"<unk>"});
+        }
+
+        {
+            auto r = tokenize(bpe, "accb");
+            assert(r.size() == 4);
+            assert(r[0] == ustr{"a"});
+            assert(r[1] == ustr{"<unk>"});
+            assert(r[2] == ustr{"<unk>"});
+            assert(r[3] == ustr{"b"});
+        }
+    }
+
+    // fused <unk>
+    {
+        berts::bpe bpe{"<unk>", 0.0, true};
+        std::unordered_map<ustr, size_t> vocab{{
+            {ustr{"a"}, 0},
+            {ustr{"b"}, 1},
+        }};
+        std::vector<std::pair<ustr, ustr>> merge{};
+        bool ok = bpe.load_vocab(vocab, merge);
+        assert(ok);
+
+        {
+            auto r = tokenize(bpe, "c");
+            assert(r.size() == 1);
+            assert(r[0] == ustr{"<unk>"});
+        }
+
+        {
+            auto r = tokenize(bpe, "cc");
+            assert(r.size() == 2);
+            assert(r[0] == ustr{"<unk>"});
+            assert(r[1] == ustr{"<unk>"});
+        }
+
+        {
+            auto r = tokenize(bpe, "accb");
+            assert(r.size() == 3);
+            assert(r[0] == ustr{"a"});
+            assert(r[1] == ustr{"<unk>"});
+            assert(r[2] == ustr{"b"});
+        }
+    }
+
+    // merge and dropout
+    {
+        berts::bpe bpe{"<unk>", 0.0};
+        std::unordered_map<ustr, size_t> vocab{{
+            {ustr{"u"}, 0},
+            {ustr{"n"}, 1},
+            {ustr{"r"}, 2},
+            {ustr{"e"}, 3},
+            {ustr{"l"}, 4},
+            {ustr{"a"}, 5},
+            {ustr{"t"}, 6},
+            {ustr{"d"}, 7},
+            {ustr{"re"}, 8},
+            {ustr{"at"}, 9},
+            {ustr{"ed"}, 10},
+            {ustr{"un"}, 11},
+            {ustr{"ated"}, 12},
+            {ustr{"rel"}, 13},
+            {ustr{"related"}, 14},
+            {ustr{"unrelated"}, 15},
+        }};
+        std::vector<std::pair<ustr, ustr>> merge{{
+            {"r", "e"},
+            {"a", "t"},
+            {"e", "d"},
+            {"u", "n"},
+            {"at", "ed"},
+            {"re", "l"},
+            {"rel", "ated"},
+            {"un", "related"},
+        }};
+        bool ok = bpe.load_vocab(vocab, merge);
+        assert(ok);
+
+        {
+            auto r = tokenize(bpe, "unrelated");
+            assert(r.size() == 1);
+            assert(r[0] == ustr{"unrelated"});
+        }
+
+        {
+            bpe.dropout(1.0);
+            auto r = tokenize(bpe, "unrelated");
+            assert(r.size() == 9);
+            assert(r[0] == ustr{"u"});
+            assert(r[1] == ustr{"n"});
+            assert(r[2] == ustr{"r"});
+            assert(r[3] == ustr{"e"});
+            assert(r[4] == ustr{"l"});
+            assert(r[5] == ustr{"a"});
+            assert(r[6] == ustr{"t"});
+            assert(r[7] == ustr{"e"});
+            assert(r[8] == ustr{"d"});
+        }
+
+        {
+            bpe.dropout(0.5);
+            auto r = tokenize(bpe, "unrelated");
+            assert(0 < r.size());
+            assert(r.size() < 9);
+        }
+    }
+
+    // out of vocabulary in merges
+    {
+        berts::bpe bpe{"<unk>", 0.0, false};
+        std::unordered_map<ustr, size_t> vocab{{
+            {ustr{"a"}, 0},
+            {ustr{"b"}, 1},
+            {ustr{"c"}, 2},
+            {ustr{"ab"}, 3},
+        }};
+        std::vector<std::pair<ustr, ustr>> merge{};
+        bool ok = bpe.load_vocab(vocab, merge);
+        assert(!ok);
+    }
+
+    return 0;
+}
